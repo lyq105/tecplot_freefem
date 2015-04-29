5358d4222c4d0d0609adac046cfe3b6c26bce298
liyiqiang <lyq105@163.com>
2015-04-29 12:34:57 +0800

    1. 添加了一些新的命令可以支持默认变量名；
    2. 可以同时输出两个到四个变量的文件；

--- fftecplot.cpp
+++ fftecplot.cpp
@@ -2,13 +2,17 @@
 #include "AFunction_ext.hpp"
 #include "TECIO.h"
 using namespace Fem2D;
+static string default_name1 = "ud";
+static string default_name2 = "ud vd ";
+static string default_name3 = "ud vd wd ";
+static string default_name4 = "ud vd wd pd";
 double savetec(string* const& file, Mesh* const& pTh,KN<double>* const& uh,string* const& varname)
 {
 	string shape;
   ofstream pf((*file).c_str());
   size_t n, m;
   Mesh& Th = *pTh;
-  
+
   pf << "TITLE = \" \"\n";
   pf << "VARIABLES = \"X\", \"Y\"";
   if (Th.dim==3)
@@ -23,25 +27,25 @@ double savetec(string* const& file, Mesh* const& pTh,KN<double>* const& uh,strin
 	  shape = "LINESEG";
 	  }
   */
-  
+
   else if (Th.dim==3)
 	 {
 	   m = 4;
 	   shape = "TETRAHEDRON";
 	 }
-  
+
   pf << "ZONE N=" << Th.nv  << ", E=" << Th.nt << ", F=FEPOINT, ET=" << shape << endl;
   for (int i=0;i<Th.nv;i++)
     pf << std::setprecision(5) << setw(18) << (R2) Th(i)  << " " << (*uh)[i] <<" \n" ;
-  
-  
+
+
   for (int k=0;k<Th.nt;++k)
     {
       for (n=0; n<m; n++)
 	pf << Th(k,n)+1  << "  ";
       pf << endl;
     }
-  
+
   pf.close();
 	return 0;
 }
@@ -51,7 +55,7 @@ double saveTecplot(string* const& file, Mesh* const& pTh,KN<double>* const& uh)
   ofstream pf((*file).c_str());
   size_t n, m;
   Mesh& Th = *pTh;
-  
+
   pf << "TITLE = \" \"\n";
   pf << "VARIABLES = \"X\", \"Y\"";
   if (Th.dim==3)
@@ -66,33 +70,38 @@ double saveTecplot(string* const& file, Mesh* const& pTh,KN<double>* const& uh)
 	  shape = "LINESEG";
 	  }
   */
-  
+
   else if (Th.dim==3)
 	 {
 	   m = 4;
 	   shape = "TETRAHEDRON";
 	 }
-  
+
   pf << "ZONE N=" << Th.nv  << ", E=" << Th.nt << ", F=FEPOINT, ET=" << shape << endl;
   for (int i=0;i<Th.nv;i++)
     pf << std::setprecision(5) << setw(18) << (R2) Th(i)  << " " << (*uh)[i] <<" \n" ;
-  
-  
+
+
   for (int k=0;k<Th.nt;++k)
     {
       for (n=0; n<m; n++)
 	pf << Th(k,n)+1  << "  ";
       pf << endl;
     }
-  
+
   pf.close();
 	return 0;
 }
 
-double savetec_b(string* const& file, Mesh* const& pTh,KN<double>* const& uh, string* const& valname)
+double savetec_b(
+                 string* const& file,
+                 Mesh* const& pTh,
+                 KN<double>* const& uh,
+                 string* const& varname =  &default_name1
+                 )
 {
   // 提取网格数据
-	
+
 	Mesh& th = *pTh;
 	double* x = new double[th.nv];
 	double* y = new double[th.nv];
@@ -106,27 +115,26 @@ double savetec_b(string* const& file, Mesh* const& pTh,KN<double>* const& uh, st
 		//cout << endl;
 	}
 
-	// data 
-	
+	// data
+
 	for (int j=0;j<th.nv;j++){
 		x[j] = th.v(j).x;
 		y[j] = th.v(j).y;
-		u[j] = (*uh)[j];
-		//cout << data[0*th.nv + j] << " "	;	
-		//cout << data[1*th.nv + j] << " "	;	
-		//cout << data[2*th.nv + j] << endl;		
+		//u[j] = (*uh)[j];
 	}
 
 	INTEGER4 Debug = 1;
 	INTEGER4 VIsDouble = 0;
 	INTEGER4 FileFormat = 0; // 0 == PLT, 1 == SZPLT
 	INTEGER4 FileType = 0;
-	INTEGER4 I = 0; 
-
+	INTEGER4 I = 0;
+	cout << "============================" << endl;
+	cout << *varname <<endl;
+	cout << "============================" << endl;
 	// 设置
 	I = TECINI142(
 			(char*)"Freefem++2tecplot",
-			(char*)"X Y U",
+			("X Y "+ *varname).c_str(),
 			file->c_str(),
 			(char*)".",
 			&FileFormat,
@@ -164,13 +172,13 @@ double savetec_b(string* const& file, Mesh* const& pTh,KN<double>* const& uh, st
 			&np,
 			&nt,
 			&KMxOrNumFaces ,
-			&ICellMax, 
+			&ICellMax,
 			&JCellMax,
 			&KCellMax,
 			&SolutionTime,
 			&StrandID,
 			&ParentZone,
-			&IsBlock, 
+			&IsBlock,
 			&NumFaceConnections,
 			&FaceNeighborMode,
 			&TotalNumFaceNodes,
@@ -189,7 +197,7 @@ double savetec_b(string* const& file, Mesh* const& pTh,KN<double>* const& uh, st
 	// 填入数据 有限元数据或顺序数据
 	I = TECDAT142(&N,x,&isdouble);
 	I = TECDAT142(&N,y,&isdouble);
-	I = TECDAT142(&N,u,&isdouble);
+	I = TECDAT142(&N,*uh,&isdouble);
 	//cout << "dat" << endl;
 			//or TECNODE142  // 填入有限元网格数据
 	I = TECNOD142(cont);
@@ -198,27 +206,395 @@ double savetec_b(string* const& file, Mesh* const& pTh,KN<double>* const& uh, st
 
 	delete [] x;
 	delete [] y;
-	delete [] u;
+//	delete [] u;
 	delete [] cont;
 	return 1;
 }
 
-//   add the function name to the freefem++ table 
+double savetec_b2(
+		string* const& file,
+		Mesh* const& pTh,
+		KN<double>* const& u1,
+		KN<double>* const& u2,
+	 	string* const& varname = &default_name2
+		)
+{
+  // 提取网格数据
+
+	Mesh& th = *pTh;
+	double* x = new double[th.nv];
+	double* y = new double[th.nv];
+//	double* u = new double[th.nv];
+ 	INTEGER4* cont = new INTEGER4[3*th.nt];
+	for (int j=0;j<th.nt;j++){
+		for (int i=0;i<3;i++){
+			cont[j*3+i]=th(j,i)+1;
+		//	cout<< cont[j*3+i] << " ";
+		}
+		//cout << endl;
+	}
+
+	// data
+
+	for (int j=0;j<th.nv;j++){
+		x[j] = th.v(j).x;
+		y[j] = th.v(j).y;
+		//u[j] = (*uh)[j];
+	}
+
+	INTEGER4 Debug = 1;
+	INTEGER4 VIsDouble = 0;
+	INTEGER4 FileFormat = 0; // 0 == PLT, 1 == SZPLT
+	INTEGER4 FileType = 0;
+	INTEGER4 I = 0;
+	cout << "============================" << endl;
+	cout << *varname <<endl;
+	cout << "============================" << endl;
+	// 设置
+	I = TECINI142(
+			(char*)"Freefem++2tecplot",
+			("X Y "+ *varname).c_str(),
+			file->c_str(),
+			(char*)".",
+			&FileFormat,
+			&FileType,
+			&Debug,
+			&VIsDouble
+			);
+	INTEGER4 zonetype = 2;
+	INTEGER4 np = th.nv;
+	INTEGER4 nt = th.nt;
+	INTEGER4 KMxOrNumFaces = 0;
+	INTEGER4 ICellMax = 0;
+	INTEGER4 JCellMax = 0;
+	INTEGER4 KCellMax = 0;
+	double SolutionTime= 0.0;
+	INTEGER4 StrandID = 0;
+	INTEGER4 ParentZone = 0;
+	INTEGER4 IsBlock = 1;
+	INTEGER4 NumFaceConnections = 0;
+	INTEGER4 FaceNeighborMode = 0;
+	INTEGER4 TotalNumFaceNodes = 0;
+	INTEGER4 NumConnectedBoundaryFaces = 0;
+	INTEGER4 TotalNumBoundaryConnections = 0;
+	INTEGER4 PassiveVarList;
+	INTEGER4 ValueLocation;
+	INTEGER4 ShareVarFromZone = 0;
+	INTEGER4 ShareConnectivityFromZone = 0;
+	//cout << "init" << endl;
+	//cout << np <<endl;
+	//cout << th.nt <<endl;
+
+	I = TECZNE142(  // 设置zone的属性
+			file->c_str(),
+			&zonetype,
+			&np,
+			&nt,
+			&KMxOrNumFaces ,
+			&ICellMax,
+			&JCellMax,
+			&KCellMax,
+			&SolutionTime,
+			&StrandID,
+			&ParentZone,
+			&IsBlock,
+			&NumFaceConnections,
+			&FaceNeighborMode,
+			&TotalNumFaceNodes,
+			&NumConnectedBoundaryFaces,
+			&TotalNumBoundaryConnections,
+			NULL,
+			NULL,
+			&ShareVarFromZone,
+			&ShareConnectivityFromZone
+			);
+	//cout << "zone" << endl;
+	INTEGER4 N=np;
+	INTEGER4 isdouble=1;
+
+
+	// 填入数据 有限元数据或顺序数据
+	I = TECDAT142(&N,x,&isdouble);
+	I = TECDAT142(&N,y,&isdouble);
+	I = TECDAT142(&N,*u1,&isdouble);
+	I = TECDAT142(&N,*u2,&isdouble);
+	//cout << "dat" << endl;
+			//or TECNODE142  // 填入有限元网格数据
+	I = TECNOD142(cont);
+	//cout << "nod" << endl;
+	I = TECEND142();
+
+	delete [] x;
+	delete [] y;
+//	delete [] u;
+	delete [] cont;
+	return 1;
+}
+double savetec_b3(
+		string* const& file,
+		Mesh* const& pTh,
+		KN<double>* const& u1,
+		KN<double>* const& u2,
+		KN<double>* const& u3,
+	 	string* const& varname = &default_name3
+		)
+{
+  // 提取网格数据
+
+	Mesh& th = *pTh;
+	double* x = new double[th.nv];
+	double* y = new double[th.nv];
+	//double* u = new double[th.nv];
+ 	INTEGER4* cont = new INTEGER4[3*th.nt];
+	for (int j=0;j<th.nt;j++){
+		for (int i=0;i<3;i++){
+			cont[j*3+i]=th(j,i)+1;
+		//	cout<< cont[j*3+i] << " ";
+		}
+		//cout << endl;
+	}
+
+	// data
+
+	for (int j=0;j<th.nv;j++){
+		x[j] = th.v(j).x;
+		y[j] = th.v(j).y;
+		//u[j] = (*uh)[j];
+	}
+
+	INTEGER4 Debug = 1;
+	INTEGER4 VIsDouble = 0;
+	INTEGER4 FileFormat = 0; // 0 == PLT, 1 == SZPLT
+	INTEGER4 FileType = 0;
+	INTEGER4 I = 0;
+	cout << "============================" << endl;
+	cout << *varname <<endl;
+	cout << "============================" << endl;
+	// 设置
+	I = TECINI142(
+			(char*)"Freefem++2tecplot",
+			("X Y "+ *varname).c_str(),
+			file->c_str(),
+			(char*)".",
+			&FileFormat,
+			&FileType,
+			&Debug,
+			&VIsDouble
+			);
+	INTEGER4 zonetype = 2;
+	INTEGER4 np = th.nv;
+	INTEGER4 nt = th.nt;
+	INTEGER4 KMxOrNumFaces = 0;
+	INTEGER4 ICellMax = 0;
+	INTEGER4 JCellMax = 0;
+	INTEGER4 KCellMax = 0;
+	double SolutionTime= 0.0;
+	INTEGER4 StrandID = 0;
+	INTEGER4 ParentZone = 0;
+	INTEGER4 IsBlock = 1;
+	INTEGER4 NumFaceConnections = 0;
+	INTEGER4 FaceNeighborMode = 0;
+	INTEGER4 TotalNumFaceNodes = 0;
+	INTEGER4 NumConnectedBoundaryFaces = 0;
+	INTEGER4 TotalNumBoundaryConnections = 0;
+	INTEGER4 PassiveVarList;
+	INTEGER4 ValueLocation;
+	INTEGER4 ShareVarFromZone = 0;
+	INTEGER4 ShareConnectivityFromZone = 0;
+	//cout << "init" << endl;
+	//cout << np <<endl;
+	//cout << th.nt <<endl;
+
+	I = TECZNE142(  // 设置zone的属性
+			file->c_str(),
+			&zonetype,
+			&np,
+			&nt,
+			&KMxOrNumFaces ,
+			&ICellMax,
+			&JCellMax,
+			&KCellMax,
+			&SolutionTime,
+			&StrandID,
+			&ParentZone,
+			&IsBlock,
+			&NumFaceConnections,
+			&FaceNeighborMode,
+			&TotalNumFaceNodes,
+			&NumConnectedBoundaryFaces,
+			&TotalNumBoundaryConnections,
+			NULL,
+			NULL,
+			&ShareVarFromZone,
+			&ShareConnectivityFromZone
+			);
+	//cout << "zone" << endl;
+	INTEGER4 N=np;
+	INTEGER4 isdouble=1;
+
+
+	// 填入数据 有限元数据或顺序数据
+	I = TECDAT142(&N,x,&isdouble);
+	I = TECDAT142(&N,y,&isdouble);
+	I = TECDAT142(&N,*u1,&isdouble);
+	I = TECDAT142(&N,*u2,&isdouble);
+	I = TECDAT142(&N,*u3,&isdouble);
+	//cout << "dat" << endl;
+			//or TECNODE142  // 填入有限元网格数据
+	I = TECNOD142(cont);
+	//cout << "nod" << endl;
+	I = TECEND142();
+
+	delete [] x;
+	delete [] y;
+	//delete [] u;
+	delete [] cont;
+	return 1;
+}
+double savetec_b4(
+		string* const& file,
+		Mesh* const& pTh,
+		KN<double>* const& u1,
+		KN<double>* const& u2,
+		KN<double>* const& u3,
+		KN<double>* const& u4,
+	 	string* const& varname = &default_name4
+		)
+{
+  // 提取网格数据
+
+	Mesh& th = *pTh;
+	double* x = new double[th.nv];
+	double* y = new double[th.nv];
+	//double* z = new double[th.nv];
+ 	INTEGER4* cont = new INTEGER4[3*th.nt];
+	for (int j=0;j<th.nt;j++){
+		for (int i=0;i<3;i++){
+			cont[j*3+i]=th(j,i)+1;
+		//	cout<< cont[j*3+i] << " ";
+		}
+		//cout << endl;
+	}
+
+	// data
+
+	for (int j=0;j<th.nv;j++){
+		x[j] = th.v(j).x;
+		y[j] = th.v(j).y;
+		//z[j] = th.v(j).z;
+		//u[j] = (*uh)[j];
+	}
+
+	INTEGER4 Debug = 1;
+	INTEGER4 VIsDouble = 0;
+	INTEGER4 FileFormat = 0; // 0 == PLT, 1 == SZPLT
+	INTEGER4 FileType = 0;
+	INTEGER4 I = 0;
+	cout << "============================" << endl;
+	cout << *varname <<endl;
+	cout << "============================" << endl;
+	// 设置
+	I = TECINI142(
+			(char*)"Freefem++2tecplot",
+			("X Y "+ *varname).c_str(),
+			file->c_str(),
+			(char*)".",
+			&FileFormat,
+			&FileType,
+			&Debug,
+			&VIsDouble
+			);
+	INTEGER4 zonetype = 2;
+	INTEGER4 np = th.nv;
+	INTEGER4 nt = th.nt;
+	INTEGER4 KMxOrNumFaces = 0;
+	INTEGER4 ICellMax = 0;
+	INTEGER4 JCellMax = 0;
+	INTEGER4 KCellMax = 0;
+	double SolutionTime= 0.0;
+	INTEGER4 StrandID = 0;
+	INTEGER4 ParentZone = 0;
+	INTEGER4 IsBlock = 1;
+	INTEGER4 NumFaceConnections = 0;
+	INTEGER4 FaceNeighborMode = 0;
+	INTEGER4 TotalNumFaceNodes = 0;
+	INTEGER4 NumConnectedBoundaryFaces = 0;
+	INTEGER4 TotalNumBoundaryConnections = 0;
+	INTEGER4 PassiveVarList;
+	INTEGER4 ValueLocation;
+	INTEGER4 ShareVarFromZone = 0;
+	INTEGER4 ShareConnectivityFromZone = 0;
+	//cout << "init" << endl;
+	//cout << np <<endl;
+	//cout << th.nt <<endl;
+
+	I = TECZNE142(  // 设置zone的属性
+			file->c_str(),
+			&zonetype,
+			&np,
+			&nt,
+			&KMxOrNumFaces ,
+			&ICellMax,
+			&JCellMax,
+			&KCellMax,
+			&SolutionTime,
+			&StrandID,
+			&ParentZone,
+			&IsBlock,
+			&NumFaceConnections,
+			&FaceNeighborMode,
+			&TotalNumFaceNodes,
+			&NumConnectedBoundaryFaces,
+			&TotalNumBoundaryConnections,
+			NULL,
+			NULL,
+			&ShareVarFromZone,
+			&ShareConnectivityFromZone
+			);
+	//cout << "zone" << endl;
+	INTEGER4 N=np;
+	INTEGER4 isdouble=1;
+
+
+	// 填入数据 有限元数据或顺序数据
+	I = TECDAT142(&N,x,&isdouble);
+	I = TECDAT142(&N,y,&isdouble);
+	I = TECDAT142(&N,*u1,&isdouble);
+	I = TECDAT142(&N,*u2,&isdouble);
+	I = TECDAT142(&N,*u3,&isdouble);
+	I = TECDAT142(&N,*u4,&isdouble);
+	//cout << "dat" << endl;
+			//or TECNODE142  // 填入有限元网格数据
+	I = TECNOD142(cont);
+	//cout << "nod" << endl;
+	I = TECEND142();
+
+	delete [] x;
+	delete [] y;
+//	delete [] u;
+	delete [] cont;
+	return 1;
+}
+
+//   add the function name to the freefem++ table
 class Init { public:
 	Init();
 };
 LOADINIT(Init);
 Init::Init(){
 	// Add function with 3 arguments
-	//  Global.Add("funcs1","(",new OneOperator1s_<double, double>(funcs1)); 
-	//  Global.Add("funcs2","(",new OneOperator2s_<double, double, double >(funcs2)); 
-	//  Global.Add("funcs3","(",new OneOperator3s_<double, double, double, double  >(funcs3)); 
-	Global.Add("savetec","(",new OneOperator3_<double, string*, Mesh*,KN<double>*>(saveTecplot)); 
-	Global.Add("savetec","(",new OneOperator4_<double, string*, Mesh*,KN<double>*,string*>(savetec)); 
-	Global.Add("savetecb","(",new OneOperator4_<double, string*, Mesh*,KN<double>*,string*>(savetec_b)); 
-	//Global.Add("CppModTemplate4","(",new OneOperator4_<double, KN<double>*, KN<double>*, KN<double>*, KN<double>*>(CppModTemplate4)); 
-	//Global.Add("CppModTemplate5","(",new OneOperator5_<double, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*>(CppModTemplate5)); 
-	//Global.Add("CppModTemplate6","(",new OneOperator6_<double, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*>(CppModTemplate6)); 
-	//Global.Add("CppModTemplate7","(",new OneOperator7_<double, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*>(CppModTemplate7)); 
-	//Global.Add("CppModTemplate8","(",new OneOperator8_<double, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*>(CppModTemplate8)); 
+	//  Global.Add("funcs1","(",new OneOperator1s_<double, double>(funcs1));
+	//  Global.Add("funcs2","(",new OneOperator2s_<double, double, double >(funcs2));
+	//  Global.Add("funcs3","(",new OneOperator3s_<double, double, double, double  >(funcs3));
+	Global.Add("savetec","(",new OneOperator3_<double, string*, Mesh*,KN<double>*>(saveTecplot));
+	Global.Add("savetec","(",new OneOperator4_<double, string*, Mesh*,KN<double>*,string*>(savetec));
+	Global.Add("savetecb","(",new OneOperator4_<double, string*, Mesh*,KN<double>*,string*>(savetec_b));
+	Global.Add("savetec2b","(",new OneOperator5_<double, string*, Mesh*,KN<double>*,KN<double>*,string*>(savetec_b2));
+	Global.Add("savetec3b","(",new OneOperator6_<double, string*, Mesh*,KN<double>*,KN<double>*,KN<double>*,string*>(savetec_b3));
+	Global.Add("savetec4b","(",new OneOperator7_<double, string*, Mesh*,KN<double>*,KN<double>*,KN<double>*,KN<double>*,string*>(savetec_b4));
+	//Global.Add("CppModTemplate4","(",new OneOperator4_<double, KN<double>*, KN<double>*, KN<double>*, KN<double>*>(CppModTemplate4));
+	//Global.Add("CppModTemplate5","(",new OneOperator5_<double, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*>(CppModTemplate5));
+	//Global.Add("CppModTemplate6","(",new OneOperator6_<double, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*>(CppModTemplate6));
+	//Global.Add("CppModTemplate7","(",new OneOperator7_<double, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*>(CppModTemplate7));
+	//Global.Add("CppModTemplate8","(",new OneOperator8_<double, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*, KN<double>*>(CppModTemplate8));
 }
